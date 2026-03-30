c90e77bbb0ef58e70a6ad1975f650e046779cfdf
Railly <railly_2009@hotmail.com>
2026-03-30 07:29:42 -0500

    feat: wire all new commands and bump to v0.2.1
    Register player, top, library, track, artist, history command groups.
    22 commands total (up from 8). Version 0.2.1.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,32 @@
 import { Command } from "commander";
 import { authCommand } from "./commands/auth.js";
+import {
+	artistAlbumsCommand,
+	artistGetCommand,
+	artistRelatedCommand,
+	artistTopTracksCommand,
+} from "./commands/artist.js";
 import { createCommand } from "./commands/create.js";
+import { historyCommand } from "./commands/history.js";
+import {
+	libraryCheckCommand,
+	libraryListCommand,
+	libraryRemoveCommand,
+	librarySaveCommand,
+} from "./commands/library.js";
 import { meCommand } from "./commands/me.js";
+import {
+	playerDevicesCommand,
+	playerNextCommand,
+	playerNowCommand,
+	playerPauseCommand,
+	playerPlayCommand,
+	playerPrevCommand,
+	playerQueueCommand,
+	playerRepeatCommand,
+	playerShuffleCommand,
+	playerVolumeCommand,
+} from "./commands/player.js";
 import {
 	playlistAddCommand,
 	playlistGetCommand,
@@ -9,19 +34,22 @@ import {
 } from "./commands/playlist.js";
 import { recommendCommand } from "./commands/recommend.js";
 import { searchCommand } from "./commands/search.js";
+import { topArtistsCommand, topTracksCommand } from "./commands/top.js";
+import { trackFeaturesCommand, trackGetCommand } from "./commands/track.js";
 
 const program = new Command();
 
 program
 	.name("spoti-cli")
 	.description("Spotify Web API from your terminal")
-	.version("0.1.0");
+	.version("0.2.0");
 
 program
 	.command("auth")
 	.description("Authenticate with Spotify (OAuth2 PKCE)")
 	.option("-c, --client-id <id>", "Spotify app client ID")
-	.action((opts) => authCommand(opts.clientId));
+	.option("-u, --upgrade", "Re-authenticate with expanded permissions", false)
+	.action((opts) => authCommand(opts.clientId, opts.upgrade));
 
 program
 	.command("search <query>")
@@ -63,6 +91,171 @@ program
 	.description("Show current user profile")
 	.option("--json", "Output as JSON", false)
 	.action((opts) => meCommand(opts));
+
+program
+	.command("history")
+	.description("Recently played tracks")
+	.option("-l, --limit <n>", "Number of tracks (max 50)", "20")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => historyCommand(opts));
+
+// --- player ---
+
+const player = program.command("player").description("Control playback");
+
+player
+	.command("now")
+	.description("Show currently playing track")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => playerNowCommand(opts));
+
+player
+	.command("play")
+	.description("Start or resume playback")
+	.option("--uri <uri>", "Track, album, or playlist URI to play")
+	.option("--device <id>", "Device ID to play on")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => playerPlayCommand(opts));
+
+player
+	.command("pause")
+	.description("Pause playback")
+	.action(() => playerPauseCommand());
+
+player
+	.command("next")
+	.description("Skip to next track")
+	.action(() => playerNextCommand());
+
+player
+	.command("prev")
+	.description("Skip to previous track")
+	.action(() => playerPrevCommand());
+
+player
+	.command("queue <uri>")
+	.description("Add a track to the queue")
+	.action((uri) => playerQueueCommand(uri));
+
+player
+	.command("devices")
+	.description("List available devices")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => playerDevicesCommand(opts));
+
+player
+	.command("volume <percent>")
+	.description("Set volume (0-100)")
+	.action((percent) => playerVolumeCommand(percent));
+
+player
+	.command("shuffle <state>")
+	.description("Toggle shuffle (on/off)")
+	.action((state) => playerShuffleCommand(state));
+
+player
+	.command("repeat <state>")
+	.description("Set repeat mode (off/track/context)")
+	.action((state) => playerRepeatCommand(state));
+
+// --- top ---
+
+const top = program.command("top").description("Your top tracks and artists");
+
+top
+	.command("tracks")
+	.description("Your most listened tracks")
+	.option("-r, --range <range>", "Time range: short_term, medium_term, long_term", "medium_term")
+	.option("-l, --limit <n>", "Number of results", "20")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => topTracksCommand(opts));
+
+top
+	.command("artists")
+	.description("Your most listened artists")
+	.option("-r, --range <range>", "Time range: short_term, medium_term, long_term", "medium_term")
+	.option("-l, --limit <n>", "Number of results", "20")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => topArtistsCommand(opts));
+
+// --- library ---
+
+const library = program.command("library").description("Manage saved tracks");
+
+library
+	.command("list")
+	.description("List saved tracks")
+	.option("-l, --limit <n>", "Number of tracks", "20")
+	.option("--offset <n>", "Offset for pagination", "0")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => libraryListCommand(opts));
+
+library
+	.command("save")
+	.description("Save tracks to library")
+	.requiredOption("--tracks <ids>", "Comma-separated track IDs or URIs")
+	.action((opts) => librarySaveCommand(opts));
+
+library
+	.command("remove")
+	.description("Remove tracks from library")
+	.requiredOption("--tracks <ids>", "Comma-separated track IDs or URIs")
+	.action((opts) => libraryRemoveCommand(opts));
+
+library
+	.command("check")
+	.description("Check if tracks are in library")
+	.requiredOption("--tracks <ids>", "Comma-separated track IDs or URIs")
+	.option("--json", "Output as JSON", false)
+	.action((opts) => libraryCheckCommand(opts));
+
+// --- track ---
+
+const track = program.command("track").description("Track details and audio features");
+
+track
+	.command("get <id>")
+	.description("Get track details")
+	.option("--json", "Output as JSON", false)
+	.action((id, opts) => trackGetCommand(id, opts));
+
+track
+	.command("features <id>")
+	.description("Get audio features (energy, danceability, tempo, key, etc.)")
+	.option("--json", "Output as JSON", false)
+	.action((id, opts) => trackFeaturesCommand(id, opts));
+
+// --- artist ---
+
+const artist = program.command("artist").description("Artist details and discography");
+
+artist
+	.command("get <id>")
+	.description("Get artist details")
+	.option("--json", "Output as JSON", false)
+	.action((id, opts) => artistGetCommand(id, opts));
+
+artist
+	.command("top-tracks <id>")
+	.description("Get artist's top tracks")
+	.option("-m, --market <code>", "Market/country code", "US")
+	.option("--json", "Output as JSON", false)
+	.action((id, opts) => artistTopTracksCommand(id, opts));
+
+artist
+	.command("albums <id>")
+	.description("Get artist's albums")
+	.option("-l, --limit <n>", "Number of albums", "20")
+	.option("--json", "Output as JSON", false)
+	.action((id, opts) => artistAlbumsCommand(id, opts));
+
+artist
+	.command("related <id>")
+	.description("Get related artists")
+	.option("--json", "Output as JSON", false)
+	.action((id, opts) => artistRelatedCommand(id, opts));
+
+// --- playlist ---
 
 const playlist = program.command("playlist").description("Manage playlists");
 
