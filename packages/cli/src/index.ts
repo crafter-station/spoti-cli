60ff4813d36d1ff3b5433ecc2642cc260dee0a30
Railly <railly_2009@hotmail.com>
2026-03-30 07:37:42 -0500

    chore: bump to v0.3.0

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -42,7 +42,7 @@ const program = new Command();
 program
 	.name("spoti-cli")
 	.description("Spotify Web API from your terminal")
-	.version("0.2.0");
+	.version("0.3.0");
 
 program
 	.command("auth")
